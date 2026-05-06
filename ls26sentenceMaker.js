2e56bc0321ad2601f26ac9a888b4eb1c3e378281
alex <alexgeiser78@gmail.com>
2026-05-07 00:06:37 +0200

    ls26

---
+++ ls26sentenceMaker.js
@@ -0,0 +1,25 @@
+let adjective = "cool";
+let noun = "flower";
+let verb = "eat";
+let place = "boat";
+let adjective2 = "lazy";
+let noun2 = "bike";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "noisy";
+noun = "bird";
+verb = "drink";
+place = "kitchen";
+adjective2 = "smelly";
+noun2 = "short";
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
