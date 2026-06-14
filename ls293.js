242622798f0ece52773aaed2d957958517daeb54
alex <alexgeiser78@gmail.com>
2026-06-14 14:24:52 +0200

    ls293

--- ls293.js
+++ ls293.js
@@ -1,1 +1,13 @@
 const squad = [];
+
+const firstAstronaut = {
+  id: 1,
+  name: "Andy",
+  role: "Commander",
+  isEVAEligible: true,
+  priority: 3
+};
+
+function addCrewMember(crew, astronaut) {
+
+}
