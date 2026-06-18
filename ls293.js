012bd313b96c0f4ecbd58f4b04c8b25fa0dbba20
alex <alexgeiser78@gmail.com>
2026-06-18 11:51:33 +0200

    ls293

--- ls293.js
+++ ls293.js
@@ -49,10 +49,11 @@ function swapCrewMembers(crew, fromIndex, toIndex) {
 
   const updatedCrew = crew.slice();
   updatedCrew[fromIndex] = updatedCrew.splice(toIndex, 1, updatedCrew[fromIndex])[0];
-
-for (let i = 0; i < updatedCrew.length; i++) {
+  for (let i = 0; i < updatedCrew.length; i++) {
     console.log(updatedCrew[i].name);
   }
 
-  return updatedCrew;
+  return updatedCrew; 
 }
+
+const updatedSquad = swapCrewMembers(squad, 2, 5);
