1b5ad003f83beb02faad5aa86f937922a211922f
alex <alexgeiser78@gmail.com>
2026-07-12 21:40:55 +0200

    ls325

--- ls325.js
+++ ls325.js
@@ -83,5 +83,18 @@ function groupByDecade(catalog) {
 
 const byDecade = groupByDecade(catalog);
 
-function renderEntry(entry) {}
+function renderEntry(entry) {
+  const title = entry.title || "Unknown";
+  const author = entry.author || "Unknown";
+  const year = entry.year || "Unknown";
+  const location = entry.location || "Unknown";
+
+  return `${"-".repeat(25)}
+Title: ${title}
+Author: ${author}
+Year: ${year}
+Location: ${location}
+${"-".repeat(25)}`;
+}
+
 console.log(renderEntry(catalog[0]));
